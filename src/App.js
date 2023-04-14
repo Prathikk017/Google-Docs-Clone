34c219b8a937708db16d5f538d1764ab58b0df4d
Prathik <prathikk017@gmail.com>
2023-04-14 19:36:29 +0530

    added texteditor and styles to it.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import TextEditor from "./TextEditor";
+
 
 function App() {
+  
   return (
-    <div>
-      <h1>HELLO!!!!</h1>
+    <div >
+      <TextEditor/>
     </div>
   );
 }
